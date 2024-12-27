8ae39e9f717a1ac5b6391abfecf735a99c0eadb3
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2024-12-27 12:54:46 +0100

    Add block count check to column script

--- column.lua
+++ column.lua
@@ -1,6 +1,21 @@
 local digutils2 = require 'digutils2'
 
 local select = digutils2.rememberItem()
+local count = digutils2.itemCounter()
+
+local width = digutils2.ask("Width", "number")
+local depth = digutils2.ask("Depth", "number", width)
+
+local function checkBlocks(height)
+	local needed = (height * 2 * (width + depth - 1))
+
+	if count() < needed then
+		digutils2.down(height)
+		error("Not enouhg blocks: need at least " .. tostring(needed))
+	end
+
+	checkBlocks = function() end
+end
 
 local function column()
 	local height = 0
@@ -11,6 +26,8 @@ local function column()
 			break
 		end
 	end
+
+	checkBlocks(height)
 
 	for _ = 1, height do
 		turtle.down()
@@ -32,18 +49,15 @@ local function wall(length, turn)
 	end
 end
 
-local width = digutils2.ask("Width", "number")
-local length = digutils2.ask("Length", "number", width)
-
-if (width > 2) and (length > 2) then
+if (width > 2) and (depth > 2) then
 	turtle.forward()
 	turtle.forward()
 	turtle.turnLeft()
 	turtle.turnLeft()
 
-	wall(length - 1, turtle.turnRight)
+	wall(depth - 1, turtle.turnRight)
 	wall(width - 1, turtle.turnRight)
-	wall(length - 1, turtle.turnRight)
+	wall(depth - 1, turtle.turnRight)
 	wall(width -1, turtle.turnLeft)
 else
 	error("NYI: Both dimensions must be > 2")
